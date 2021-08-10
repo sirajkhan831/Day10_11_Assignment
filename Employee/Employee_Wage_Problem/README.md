562c5d4bb089370c107137194499e6688d3f0821
Siraj <sirajkhanc2305@gmail.com>
2021-08-10 09:41:09 +0530

    Removed Employee Folder_2

--- Employee/Employee_Wage_Problem/README.md
+++
@@ -1,2 +0,0 @@
-# Employee_Wage_Computation_Problem
-shell script for Store the Day and the Daily Wage along with the Total Wage
--- Employee/Untitled-1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-declare -A sound
-sound[dog]="bark"
-sound[cat]="meow"
-sound[cow]="moah"
-
-echo ${sound[@]}
-echo ${!sound[@]}
